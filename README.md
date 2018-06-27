5a0d3d5d6c151dd9b7c30070293d8860764b146b
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-06-27 10:31:14 +0800

    add ggz install from homebrew
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# homebrew
-ggz: homebrew install
+# GGZ: Homebrew
+
+Homebrew repository to install GGZ on macOS.
+
+## Prepare
+
+```bash
+$ brew tap go-ggz/ggz
+```
+
+## Install
+
+### ggz
+
+```bash
+brew install ggz
+ggz -h
+```
+
+## Contributing
+
+Fork -> Patch -> Push -> Pull Request
+
+## License
+
+This project is under the MIT License. See the [LICENSE](LICENSE) file for the
+full license text.
+
+## Copyright
+
+```
+Copyright (c) 2018 The Bo-Yi Wu <https://twitter.com/appleboy>
+```
---
+++ ggz.rb
@@ -0,0 +1,34 @@
+require "formula"
+
+class GGZ < Formula
+  homepage "https://github.com/go-ggz/ggz"
+  head "https://github.com/go-ggz/ggz.git"
+
+  head do
+    url "https://github.com/go-ggz/ggz.git", :branch => "master"
+    depends_on "go" => :build
+  end
+
+  test do
+    system "#{bin}/ggz", "--version"
+  end
+
+  def install
+    case
+    when build.head?
+      mkdir_p buildpath/File.join("src", "github.com", "go-ggz")
+      ln_s buildpath, buildpath/File.join("src", "github.com", "go-ggz", "ggz")
+
+      ENV.append_path "PATH", File.join(buildpath, "bin")
+
+      ENV["GOPATH"] = buildpath
+      ENV["GOHOME"] = buildpath
+
+      system "cd src/github.com/go-ggz/ggz && make generate build"
+
+      bin.install "#{buildpath}/ggz" => "ggz"
+    else
+      bin.install "#{buildpath}/ggz-#{version}-darwin-10.6-amd64" => "ggz"
+    end
+  end
+end
